83d948727a351ba39cfbd06e931bdd533fd8c158
elitrout <elitrout24@gmail.com>
2015-11-27 18:53:47 +0100

    Move all data file path into path.js
    modified:   index.html
    new file:   path.js

---
+++ path.js
@@ -0,0 +1,18 @@
+// Data file path
+
+var pitchtrack_t_default = "data/teacher_regression_pitchtrack.csv",
+    pitchtrack_s_default = "data/student_regression_pitchtrack.csv",
+    noteSeg_t_default = "data/pitchtracknotetranscriptionsegmentation/weiguojia_section_pro_monoNoteOut_midi.txt",
+    noteSeg_s_default = "data/pitchtracknotetranscriptionsegmentation/weiguojia_section_amateur_monoNoteOut_midi.txt",
+    noteAlign_S2T_default = "data/amateur2pro.txt",
+    noteAlign_T2S_default = "data/pro2amateur.txt",
+    audio_t_default = "data/weiguojia_section_pro.wav",
+    audio_s_default = "data/weiguojia_section_amateur.wav",
+    pitchtrack_t_update,
+    pitchtrack_s_update = "data/teacher_regression_pitchtrack.csv",
+    noteSeg_t_update = "data/pitchtracknotetranscriptionsegmentation/weiguojia_section_pro_monoNoteOut_midi.txt",
+    noteSeg_s_update = "data/pitchtracknotetranscriptionsegmentation/weiguojia_section_amateur_monoNoteOut_midi.txt",
+    noteAlign_S2T_update = "data/amateur2pro.txt",
+    noteAlign_T2S_update = "data/pro2amateur.txt",
+    audio_t_update,
+    audio_s_update = "data/weiguojia_section_pro.wav";
